b896b05220f651fa6feeb87c30334c4cd4959fd6
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-01-23 22:33:28 +0100

    Count all tags in Tags list

--- js/script.js
+++ js/script.js
@@ -135,44 +135,41 @@
 
   const generateTagList = function () {
     /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
 
+    let allTagsCounter = {};
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
-    /* START LOOP: for every article: */
-    for (let article of articles) {
-      /* find tags wrapper */
-      const tagList = article.querySelector(optArticleTagsSelector);
 
-      /* make html variable with empty string */
-      let html = '';
+    for (let article of articles) {
       /* get tags from data-tags attribute */
       const articleTags = article.getAttribute('data-tags');
       /* split tags into array */
       const articleTagsArray = articleTags.split(' ');
       /* START LOOP: for each tag */
       for (let tag of articleTagsArray) {
-        /* generate HTML of the link */
-        const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
-        /* add generated code to html variable */
-
-        /* [NEW] check if this link is NOT already in allTags */
-        if (allTags.indexOf(linkHTML) == -1) {
-          /* [NEW] add generated code to allTags array */
-          allTags.push(linkHTML);
+        if (allTagsCounter[tag] == undefined) {
+          /* [NEW] add counter for each tag */
+          allTagsCounter[tag] = 1;
+        } else {
+          allTagsCounter[tag]++;
         }
-
-        /* END LOOP: for each tag */
       }
-      /* insert HTML of all the links into the tags wrapper */
-
-      /* END LOOP: for every article: */
     }
-    /* [NEW] find list of tags in right column */
+
+    /* construct HTML of the all the links with counts */
+    let allTags = [];
+    for (let tag in allTagsCounter) {
+      allTags.push(
+        `<li><a href="#tag-${tag}">${tag}</a> <span>(${allTagsCounter[tag]})</span></li>`
+      );
+    }
+
+    /* find list of tags in right column */
     const tagList = document.querySelector(optTagsListSelector);
 
-    /* [NEW] add html from allTags to tagList */
+    /*  add html from allTags to tagList */
     tagList.innerHTML = allTags.join('\n');
+    console.log('allTagsCounter', allTagsCounter);
   };
 
   const tagClickHandler = function (event) {
